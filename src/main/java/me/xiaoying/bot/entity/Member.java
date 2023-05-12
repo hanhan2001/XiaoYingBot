66185ff41e317f66c975161508e3d8304eec02fd
Hanhan <764932129@qq.com>
2023-05-12 21:29:48 +0800

    feat: Group user entity

--- src/main/java/me/xiaoying/bot/entity/Member.java
+++ src/main/java/me/xiaoying/bot/entity/Member.java
@@ -1,7 +1,19 @@
 package me.xiaoying.bot.entity;
 
+import me.xiaoying.bot.api.XiaoYing;
+
 public class Member extends User {
+    Group group;
     public Member(String name, long id) {
         super(name, id);
+    }
+
+    public Group getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public boolean hasPermission(String permission) {
+        return XiaoYing.getPermissionService().hasPermission(this.group, this, permission);
     }
 }
