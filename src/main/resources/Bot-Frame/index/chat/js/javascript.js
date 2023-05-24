ba114ee92b670e8c88003b6e0925d31235dcacb0
Hanhan <764932129@qq.com>
2023-05-24 11:53:38 +0800

    feat: Views' change

--- src/main/resources/Bot-Frame/index/chat/js/javascript.js
+++ src/main/resources/Bot-Frame/index/chat/js/javascript.js
@@ -4,28 +4,34 @@ var titleClass = ".content .content_chat .content_chat_box_title";
 var chatEntity = document.querySelector(chatClass);
 var titleEntity = document.querySelector(titleClass);
 
-var chatboxType = "into";
-
 window.onload = function() {
 	chatEntity = document.querySelector(chatClass);
 	titleEntity = document.querySelector(titleClass);
+
+
+　　$(document).keyup(function(event){ 
+　　　　if (event.keyCode === 13){
+			sendMessage();
+　　　　} 
+　　}); 
+	// window.addEventListener("resize", function() {
+	// 	let displayBox = document.querySelector(".content .content_chat .content_chat_box .content_chat_box_chat .display");
+	// 	displayBox.scrollTop = displayBox.scrollHeight;
+	// });
 }
-
-// function showChat() {
-// 	chatEntity.classList.add("chat_on");
-// }
-// function showTitle() {
-// }
-
-// // 用户界面
-// function setChatBox(user, type, display) {
-
-// }
 
 function showChat() {
 	chatEntity.classList.contains("chat_on") ? chatEntity.classList.remove("chat_on") : chatEntity.classList.add("chat_on");
 }
-// // 用户界面
-// function setChatBox(user, type, display) {
 
-// }
+function sendMessage() {
+	let content = document.querySelector(".content .content_chat .content_chat_box .content_chat_box_chat .chat input");
+	let chatbox = "<div class='self'><p>%text%</p><div class='self_img'></div></div>".replace("%text%", content.value);
+	
+	content.value = null;
+	let displayBox = document.querySelector(".content .content_chat .content_chat_box .content_chat_box_chat .display");
+	displayBox.insertAdjacentHTML("beforeend", chatbox);
+	let second = 0.2;
+	let distance = displayBox.scrollHeight - displayBox.scrollTop;
+	displayBox.scrollTop = displayBox.scrollHeight;
+}
